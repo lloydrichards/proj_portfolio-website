2ab71296df2359bb80a96c0f6979c226228395d8
LloydRichardsIntern <59728961+LloydRichardsIntern@users.noreply.github.com>
2020-05-20 09:24:09 +0200

    int: Setting up Next.js

---
+++ pages/experiment/001.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import Layout from "../../components/Layout";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>001 - Setting up Next.js</h2>
+        </div>
+        <div>
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,22 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from "next/link";
+import Layout from "../components/Layout";
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+  <Layout title="Home | Lloyd's Working Portfolio">
+    <h1>Lloyd's Working Portfolio</h1>
+    <ul>
+      <li>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/experiment/001">
+          <a>001 - Setting Up with Next.js</a>
+        </Link>
+      </li>
+    </ul>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;

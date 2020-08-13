4bfe8e95319b85faa09859b752f18ff7504232cc
Lloyd Richards <59728961+LloydRichardsIntern@users.noreply.github.com>
2020-08-13 11:42:28 +0200

    content: SEO Content

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -6,19 +6,40 @@ import Contact from '../Contact';
 
 type Props = {
   title?: string;
+  description?: string;
+  previewImage?: string;
+  currentURL?: string;
 };
+
+const twitterHandle = '@LRichardsDesign';
+const siteName = 'Lloyd Richards Design';
 
 const Layout: React.FunctionComponent<Props> = ({
   children,
-  title = 'This is the default title',
+  title = 'Lloyd Richards Design',
+  description = 'Welcome to the digital portfolio of Lloyd Richards.  Find projects, blogs and experiments in data visualization and garden design.',
+  previewImage = '',
+  currentURL = 'https://www.lloydrichardsdesing.com',
 }) => (
   <div style={{ margin: 'auto', maxWidth: '1080px' }}>
     <Head>
       <title>{title}</title>
+      <meta name='description' content={description} />
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+
+      {/* Twitter */}
+      <meta name='twitter:card' content='summary' key='twcard' />
+      <meta name='twitter:creator' content={twitterHandle} key='twhandle' />
+
+      {/* Open Graph */}
+      <meta property='og:url' content={currentURL} key='ogurl' />
+      <meta property='og:image' content={previewImage} key='ogimage' />
+      <meta property='og:site_name' content={siteName} key='ogsitename' />
+      <meta property='og:title' content={title} key='ogtitle' />
+      <meta property='og:description' content={description} key='ogdesc' />
     </Head>
-    <div style={{ position: 'relative', height: 50, zIndex: 9, width: '100%',  }}>
+    <div style={{ position: 'relative', height: 50, zIndex: 9, width: '100%' }}>
       <FullWidthBackground />
       <Navbar />
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,105 +13,114 @@ import Projects from '../components/Projects';
 import Blog from '../components/Blog';
 import { Grid } from '@material-ui/core';
 import CV from '../components/CV';
+import { useRouter } from 'next/router';
 
-const IndexPage: React.FC = () => (
-  <Layout title="Home | Lloyd's Working Portfolio">
-    <div
-      className='Intro'
-      style={{
-        width: '100%',
-        height: '95vh',
-        background: '#F6F3F0',
-      }}
+const IndexPage: React.FC = () => {
+  const router = useRouter();
+  return (
+    <Layout
+      title='Home | Lloyd Richards Design'
+      currentURL={router.pathname}
+      previewImage={''}
     >
-      <FullWidthBackground />
-      <div style={{ position: 'absolute', top: 0, margin: '30% 0' }}>
-        <Grid container>
-          <Grid item xs={12} sm={6}></Grid>
-          <Grid item xs={12} sm={6} style={{ padding: '1em' }}>
-            <H1>Hello, I'm Lloyd</H1>
-            <Intro>
-              I'm a Zurich based <b>Data Visualization</b> and{' '}
-              <b>Garden Designer</b>. Pretty weird combinations, right? Let me
-              tell you how I got here.
-            </Intro>
-            <Button
-              style={{
-                margin: '0em',
-                textTransform: 'none',
-                color: 'black',
-                fontFamily: "'Josefin Sans', serif",
-                fontSize: '1.5em',
-              }}
-            >
-              Contact Me
-            </Button>
+      <div
+        className='Intro'
+        style={{
+          width: '100%',
+          height: '95vh',
+          background: '#F6F3F0',
+        }}
+      >
+        <FullWidthBackground />
+        <div style={{ position: 'absolute', top: 0, margin: '30% 0' }}>
+          <Grid container>
+            <Grid item xs={12} sm={6}></Grid>
+            <Grid item xs={12} sm={6} style={{ padding: '1em' }}>
+              <H1>Hello, I'm Lloyd</H1>
+              <Intro>
+                I'm a Zurich based <b>Data Visualization</b> and{' '}
+                <b>Garden Designer</b>. Pretty weird combinations, right? Let me
+                tell you how I got here.
+              </Intro>
+              <Button
+                style={{
+                  margin: '0em',
+                  textTransform: 'none',
+                  color: 'black',
+                  fontFamily: "'Josefin Sans', serif",
+                  fontSize: '1.5em',
+                }}
+              >
+                Contact Me
+              </Button>
+            </Grid>
           </Grid>
+        </div>
+      </div>
+      <div id='About' style={{ width: '100%', minHeight: '320px' }}>
+        <H2>About</H2>
+        <Grid container>
+          <Grid item xs={12} sm={6} md={8}>
+            <Content>
+              In my life, I've traveled to a lot of places, met a lot of people
+              and been many things. From a Baker in Canada; to a Bamboo Builder
+              in Costa Rica; to a Landscaper in England; to a Web Designer in
+              Switzerland. My passion for life and experiancing new things has
+              always overpowered any fears I might have about trying new things.
+              At the core of most of my decisions are three main principles:
+            </Content>
+            <ContentList>
+              <li>
+                Passionate for <b>Sustainability</b> and having a positive
+                impact on the planet
+              </li>
+              <li>
+                Obsessed about <b>System Thinking</b> and understanding the
+                interconnection of disiplines
+              </li>
+              <li>
+                Constantly <b>Learning</b> new skills, and applying to real
+                world projects.
+              </li>
+            </ContentList>
+            <Content>
+              I bring a unique perspective to the projects I work on, being able
+              to empathize with different users, seeing the problem from
+              different angles, and come up with solutions from different
+              understandings.
+            </Content>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}></Grid>
         </Grid>
       </div>
-    </div>
-    <div id='About' style={{ width: '100%', minHeight: '320px' }}>
-      <H2>About</H2>
-      <Grid container>
-        <Grid item xs={12} sm={6} md={8}>
-          <Content>
-            In my life, I've traveled to a lot of places, met a lot of people
-            and been many things. From a Baker in Canada; to a Bamboo Builder in
-            Costa Rica; to a Landscaper in England; to a Web Designer in
-            Switzerland. My passion for life and experiancing new things has
-            always overpowered any fears I might have about trying new things.
-            At the core of most of my decisions are three main principles:
-          </Content>
-          <ContentList>
-            <li>
-              Passionate for <b>Sustainability</b> and having a positive impact
-              on the planet
-            </li>
-            <li>
-              Obsessed about <b>System Thinking</b> and understanding the
-              interconnection of disiplines
-            </li>
-            <li>
-              Constantly <b>Learning</b> new skills, and applying to real world
-              projects.
-            </li>
-          </ContentList>
-          <Content>
-            I bring a unique perspective to the projects I work on, being able
-            to empathize with different users, seeing the problem from different
-            angles, and come up with solutions from different understandings.
-          </Content>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}></Grid>
-      </Grid>
-    </div>
-    <div id='Portfolio' style={{ width: '100%', minHeight: '640px' }}>
-      <H2>
-        Recent <a href='/portfolio'>/ Portfolio</a>
-      </H2>
-      <Projects />
-    </div>
-    <div
-      id='Blog'
-      style={{
-        width: '100%',
-        position: 'relative',
-        top: 0,
-        minHeight: '320px',
-        background: '#F6F3F0',
-      }}
-    >
-      <FullWidthBackground />
-      <H2>
-        Recent <a href='/blog'>/ Blog</a>
-      </H2>
-      <Blog />
-    </div>
-    <div id='CV' style={{ width: '100%', minHeight: '320px' }}>
-      <H2>CV</H2>
-      <CV />
-    </div>
-  </Layout>
-);
+      <div id='Portfolio' style={{ width: '100%', minHeight: '640px' }}>
+        <H2>
+          Recent <a href='/portfolio'>/ Portfolio</a>
+        </H2>
+        <Projects />
+      </div>
+      <div
+        id='Blog'
+        style={{
+          width: '100%',
+          position: 'relative',
+          top: 0,
+          minHeight: '320px',
+          background: '#F6F3F0',
+        }}
+      >
+        <FullWidthBackground />
+        <H2>
+          Recent <a href='/blog'>/ Blog</a>
+        </H2>
+        <Blog />
+      </div>
+      <div id='CV' style={{ width: '100%', minHeight: '320px' }}>
+        <H2>CV</H2>
+        <CV />
+      </div>
+    </Layout>
+  );
+};
 
 export default IndexPage;

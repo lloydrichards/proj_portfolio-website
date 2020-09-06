de1cc3b3a72678668f5e0e4773142e8e6782353d
Lloyd Richards <59728961+LloydRichardsIntern@users.noreply.github.com>
2020-09-06 22:07:25 +0200

    Add Firebase Config

---
+++ libs/FirebaseConfig.tsx
@@ -0,0 +1,8 @@
+let firebaseConfig = {
+  apiKey: 'AIzaSyBmvq2Tt6DI1Kq73N2YT6uoo2bFZmX1pWo',
+  authDomain: 'mimirhome-app.firebaseapp.com',
+  databaseURL: 'https://mimirhome-app.firebaseio.com',
+  projectId: 'mimirhome-app',
+  storageBucket: 'mimirhome-app.appspot.com',
+};
+export default firebaseConfig;

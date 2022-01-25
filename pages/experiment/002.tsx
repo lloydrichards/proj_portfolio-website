d72689456ceb9fc3628fcadd8c59aeefddebf67f
lloydrichards <lloyd.d.richards@gmail.com>
2022-01-25 10:53:48 +0100

    updated temp

--- pages/experiment/002.tsx
+++ pages/experiment/002.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import Fetch from 'isomorphic-unfetch';
-import Layout from '../../components/layout/Layout';
-import { NextPage } from 'next';
+import React from "react";
+import Fetch from "isomorphic-unfetch";
+import Layout from "../../components/layout/Layout";
+import { NextPage } from "next";
 
 type Props = {
   current: any;
@@ -10,9 +10,9 @@ type Props = {
 
 const Experiment002: NextPage<Props> = (props) => {
   return (
-    <Layout title='Experiment | 002'>
+    <Layout title="Experiment | 002">
       <h2>001 - Working with API's</h2>
-      <p>The current temperature is {props.current.temp}</p>
+      <p>The current temperature is {props?.current?.temp}</p>
       <div></div>
     </Layout>
   );
@@ -20,7 +20,7 @@ const Experiment002: NextPage<Props> = (props) => {
 
 Experiment002.getInitialProps = async function () {
   const res = await Fetch(
-    'https://api.openweathermap.org/data/2.5/onecall?lat=47.3769&lon=8.5417&units=metric&exclude=daily&appid=02f471c66896a92cf74995432fba9e3c'
+    "https://api.openweathermap.org/data/2.5/onecall?lat=47.3769&lon=8.5417&units=metric&exclude=daily&appid=02f471c66896a92cf74995432fba9e3c",
   );
   const data = await res.json();
 

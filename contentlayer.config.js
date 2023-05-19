53d41fb65827e868ad0d9ca0281fc81ae8d7972a
lloydrichards <lloyd.d.richards@gmail.com>
2023-05-19 18:30:21 +0200

    docs(occupation): :memo: added occupations as mdx

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -3,7 +3,7 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypePrettyCode from "rehype-pretty-code";
 import rehypeSlug from "rehype-slug";
 import remarkGfm from "remark-gfm";
-import { CATEGORIES } from "./src/lib/const";
+import { OCCUPATION_CATEGORIES, PROJECT_CATEGORIES } from "./src/lib/const";
 
 /** @type {import('contentlayer/source-files').ComputedFields} */
 const computedFields = {
@@ -100,7 +100,7 @@ export const Project = defineDocumentType(() => ({
     },
     category: {
       type: "list",
-      of: { type: "enum", options: CATEGORIES },
+      of: { type: "enum", options: PROJECT_CATEGORIES },
       required: true,
     },
     image: {
@@ -117,9 +117,64 @@ export const Project = defineDocumentType(() => ({
   computedFields,
 }));
 
+export const Occupation = defineDocumentType(() => ({
+  name: "Occupation",
+  filePathPattern: `occupations/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    id: {
+      type: "number",
+      required: true,
+    },
+    title: {
+      type: "string",
+      required: true,
+    },
+    description: {
+      type: "string",
+    },
+    published: {
+      type: "boolean",
+      default: true,
+    },
+    company: {
+      type: "string",
+      required: true,
+    },
+    location: {
+      type: "string",
+      required: true,
+    },
+    skills: {
+      type: "list",
+      of: { type: "string" },
+    },
+    characters: {
+      type: "list",
+      of: { type: "string" },
+    },
+    category: {
+      type: "enum",
+      options: OCCUPATION_CATEGORIES,
+      required: true,
+    },
+    tags: {
+      type: "list",
+      of: { type: "string" },
+    },
+    start_date: {
+      type: "date",
+      required: true,
+    },
+    end_date: {
+      type: "date",
+    },
+  },
+}));
+
 export default makeSource({
   contentDirPath: "./src/content",
-  documentTypes: [Blog, Experiment, Project],
+  documentTypes: [Blog, Experiment, Project, Occupation],
   mdx: {
     remarkPlugins: [remarkGfm],
     rehypePlugins: [

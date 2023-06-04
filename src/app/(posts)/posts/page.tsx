e73e4105427e5a55187dda94182aaf406ce95b48
lloydrichards <lloyd.d.richards@gmail.com>
2023-06-04 13:15:37 +0200

    feat(layout): :sparkles: sidebar navigation for posts

--- src/app/(posts)/posts/page.tsx
+++ src/app/(posts)/posts/page.tsx
@@ -1,27 +1,14 @@
 import { RecentPosts } from "@/components/posts/recent_posts/RecentPosts";
 import { allBlogs, allLabs } from "contentlayer/generated";
-import Link from "next/link";
 
 const PostsPage = () => {
   const allPosts = [...allBlogs, ...allLabs].sort(
     (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
   );
   return (
-    <main className="flex min-h-screen flex-col items-center p-16">
+    <section className="flex min-h-screen flex-col items-stretch">
       <RecentPosts posts={allPosts} />
-      <section className="prose mt-8 w-full">
-        <h1 className="text-lg">All Posts</h1>
-        <ul>
-          {allPosts
-            .filter((d) => d.published)
-            .map((post) => (
-              <li key={post.slugAsParams}>
-                <Link href={post.slug}>{post.title}</Link>
-              </li>
-            ))}
-        </ul>
-      </section>
-    </main>
+    </section>
   );
 };
 

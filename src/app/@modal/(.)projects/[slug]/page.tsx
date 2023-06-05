beb55bfb077168a43944a3ca2d9b1029717d0a92
lloydrichards <lloyd.d.richards@gmail.com>
2023-06-05 21:44:35 +0200

    fix(layout): :bug: fix exported functions from pages

--- src/app/@modal/(.)projects/[slug]/page.tsx
+++ src/app/@modal/(.)projects/[slug]/page.tsx
@@ -1,10 +1,15 @@
-import {
-  ProjectPageProps,
-  getProjectFromParams,
-} from "@/app/projects/[slug]/page";
+import { ProjectPageProps } from "@/app/projects/[slug]/page";
 import { Mdx } from "@/components/Mdx";
 import { Modal } from "@/components/layout/modal/Modal";
+import { allProjects } from "contentlayer/generated";
 import Link from "next/link";
+import { notFound } from "next/navigation";
+
+const getProjectFromParams = async (slug: string) => {
+  const project = allProjects.find((project) => project.slugAsParams === slug);
+  if (!project) notFound();
+  return project;
+};
 
 const ProjectModal = async ({ params }: ProjectPageProps) => {
   const project = await getProjectFromParams(params.slug);
--- src/app/@modal/_(.)labs/[slug]/page.tsx
+++ src/app/@modal/_(.)labs/[slug]/page.tsx
@@ -1,7 +1,15 @@
-import { LabPageProps, getLabFromParams } from "@/app/(posts)/labs/[slug]/page";
+import { LabPageProps } from "@/app/(posts)/labs/[slug]/page";
 import { Mdx } from "@/components/Mdx";
 import { Modal } from "@/components/layout/modal/Modal";
+import { allLabs } from "contentlayer/generated";
 import Link from "next/link";
+import { notFound } from "next/navigation";
+
+const getLabFromParams = async (slug: string) => {
+  const lab = allLabs.find((lab) => lab.slugAsParams === slug);
+  if (!lab) notFound();
+  return lab;
+};
 
 const LabModal = async ({ params }: LabPageProps) => {
   const lab = await getLabFromParams(params.slug);

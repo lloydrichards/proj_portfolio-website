14e4047a51f6aa6dcd587f1c91cc291f0e5d3d6b
lloydrichards <lloyd.d.richards@gmail.com>
2023-07-02 17:21:08 +0200

    build(dependencies): :arrow_up: major dependency upgrades for storybook, three, hype and contentlayer

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -183,17 +183,10 @@ export default makeSource({
         rehypePrettyCode,
         {
           theme: "github-dark",
-          onVisitLine(node) {
-            // Prevent lines from collapsing in `display: grid` mode, and allow empty
-            // lines to be copy/pasted
-            if (node.children.length === 0) {
-              node.children = [{ type: "text", value: " " }];
-            }
-          },
           onVisitHighlightedLine(node) {
             node.properties.className.push("line--highlighted");
           },
-          onVisitHighlightedWord(node) {
+          onVisitHighlightedChar(node) {
             node.properties.className = ["word--highlighted"];
           },
         },

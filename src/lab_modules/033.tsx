e45dcad6d34601c4714cfb8693a95b50a5cbfdcb
lloydrichards <lloyd.d.richards@gmail.com>
2023-10-04 10:24:22 +0200

    docs(lab): :memo: update 033 Lab

--- src/lab_modules/033.tsx
+++ src/lab_modules/033.tsx
@@ -3,10 +3,12 @@ import * as O from "fp-ts/lib/Option";
 import * as E from "fp-ts/lib/Either";
 import * as T from "fp-ts/Task";
 import * as TE from "fp-ts/TaskEither";
+import * as IO from "fp-ts/IO";
 import * as t from "io-ts";
 import * as EQ from "io-ts/Eq";
 import { pipe, flow } from "fp-ts/lib/function";
 import { failure } from "io-ts/PathReporter";
+import { FC, useEffect } from "react";
 /*
  * DeepEqual of Nested Objects
  */
@@ -102,12 +104,77 @@ const getFromUrl = (url: string) =>
     TE.fold(handleErrors, (data) => T.of(data.value.name)),
   );
 
-getFromUrl("https://api.chucknorris.io/jokes/random")().then(console.log); // Output: "Invalid value undefined supplied to : { status: string, value: { id: number, name: string } }/status: string"
+// getFromUrl("https://api.chucknorris.io/jokes/random")().then(console.log); // Output: "Invalid value undefined supplied to : { status: string, value: { id: number, name: string } }/status: string"
 
 /*
  * Branching Render Logic
  */
 
+export const ConditionalRender: FC<{ value?: number }> = ({ value }) => {
+  useEffect(() => {
+    return pipe(
+      value,
+      O.fromPredicate((value) => value == 10),
+      O.fold(
+        () => IO.of(undefined), // no side effect
+        () => IO.of(console.log("The value is very important!")), // conditional side effect
+      ),
+    );
+  }, [value]);
+
+  return pipe(
+    value,
+    O.fromNullable,
+    O.fold(
+      () => <div>Loading...</div>, // Output: "Loading..."
+      (value) =>
+        pipe(
+          value,
+          E.fromPredicate(
+            (value) => value !== 0, // Addition validation
+            () => "Error: Value is not 0",
+          ),
+          E.map(
+            (value) =>
+              value > 0 ? `Positive (${value})` : `Negative (${value})`, // Transform value
+          ),
+          E.fold(
+            (error) => <div>{error}</div>, // Output: "Error: Value is not 0"
+            (success) => <div>{success}</div>, // Output: "Positive (1)"
+          ),
+        ),
+    ),
+  );
+};
+
 /*
  * Responsive Values
  */
+
+export const foldScreenWidth =
+  <T,>(
+    xs: (width: number) => T,
+    sm?: (width: number) => T,
+    md?: (width: number) => T,
+    lg?: (width: number) => T,
+  ) =>
+  (width: number) => {
+    switch (true) {
+      case width > 1200 && lg !== undefined:
+        return lg!(width);
+      case width > 992 && md !== undefined:
+        return md!(width);
+      case width > 768 && sm !== undefined:
+        return sm!(width);
+      default:
+        return xs(width);
+    }
+  };
+
+const responsiveFontSize = foldScreenWidth(
+  () => `1rem`, // default for xs screen
+  () => `1.25rem`, // sm screen
+  undefined, // md screen
+  () => `2rem`, // lg screen
+);
+// console.log(responsiveFontSize(1000)); // Output: "2rem"

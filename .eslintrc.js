ea81c738b59072828b2663af8ed0fca9068c2423
lloydrichards <lloyd.d.richards@gmail.com>
2023-10-15 12:46:25 +0200

    fix(components):

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:storybook/recommended",
+    "plugin:tailwindcss/recommended",
+  ],
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-empty-interface": "off",
+    "tailwindcss/classnames-order": "off",
+    "tailwindcss/no-custom-classname": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "no-unused-vars": "warn",
+    "prefer-const": "warn",
+    "@next/next/no-img-element": "off",
+    "no-var": "warn",
+    "@typescript-eslint/no-unused-vars": [
+      "warn", // or "error"
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+  },
+};
+module.exports = config;

fdb37aa45f9962201f18aca3c3cf65a6bd63de26
lloydrichards <lloyd.d.richards@gmail.com>
2024-02-25 16:33:05 +0100

    refactor(components): :recycle: breakout typeface styles from mdx

--- src/components/molecule/mdx.tsx
+++ src/components/molecule/mdx.tsx
@@ -2,94 +2,60 @@
 import { cn } from "@/lib/utils";
 import "@/styles/mdx.css";
 import { useMDXComponent } from "next-contentlayer/hooks";
+import {
+  typefaceHeading1,
+  typefaceHeading2,
+  typefaceHeading3,
+  typefaceHeading4,
+  typefaceHeading5,
+  typefaceHeading6,
+  typefaceAnchor,
+  typefaceBody1,
+  typefaceUList,
+  typefaceOList,
+  typefaceListItem,
+  typefaceBlockQuote,
+} from "../typeface";
 
 const components = {
   h1: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h1
-      className={cn(
-        "mt-2 scroll-m-20 text-4xl font-bold tracking-tight",
-        className,
-      )}
-      {...props}
-    />
+    <h1 className={typefaceHeading1(className)} {...props} />
   ),
   h2: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h2
-      className={cn(
-        "mt-10 scroll-m-20 border-b pb-1 text-3xl font-semibold tracking-tight first:mt-0",
-        className,
-      )}
-      {...props}
-    />
+    <h2 className={typefaceHeading2(className)} {...props} />
   ),
   h3: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h3
-      className={cn(
-        "mt-8 scroll-m-20 text-2xl font-semibold tracking-tight",
-        className,
-      )}
-      {...props}
-    />
+    <h3 className={typefaceHeading3(className)} {...props} />
   ),
   h4: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h4
-      className={cn(
-        "mt-8 scroll-m-20 text-xl font-semibold tracking-tight",
-        className,
-      )}
-      {...props}
-    />
+    <h4 className={typefaceHeading4(className)} {...props} />
   ),
   h5: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h5
-      className={cn(
-        "mt-8 scroll-m-20 text-lg font-semibold tracking-tight",
-        className,
-      )}
-      {...props}
-    />
+    <h5 className={typefaceHeading5(className)} {...props} />
   ),
   h6: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h6
-      className={cn(
-        "mt-8 scroll-m-20 text-base font-semibold tracking-tight",
-        className,
-      )}
-      {...props}
-    />
+    <h6 className={typefaceHeading6(className)} {...props} />
   ),
   a: ({ className, ...props }: React.HTMLAttributes<HTMLAnchorElement>) => (
-    <a
-      className={cn("font-medium underline underline-offset-4", className)}
-      {...props}
-    />
+    <a className={typefaceAnchor(className)} {...props} />
   ),
   p: ({ className, ...props }: React.HTMLAttributes<HTMLParagraphElement>) => (
-    <p
-      className={cn("leading-7 [&:not(:first-child)]:mt-6", className)}
-      {...props}
-    />
+    <p className={typefaceBody1(className)} {...props} />
   ),
   ul: ({ className, ...props }: React.HTMLAttributes<HTMLUListElement>) => (
-    <ul className={cn("my-6 ml-6 list-disc", className)} {...props} />
+    <ul className={typefaceUList(className)} {...props} />
   ),
   ol: ({ className, ...props }: React.HTMLAttributes<HTMLOListElement>) => (
-    <ol className={cn("my-6 ml-6 list-decimal", className)} {...props} />
+    <ol className={typefaceOList(className)} {...props} />
   ),
   li: ({ className, ...props }: React.HTMLAttributes<HTMLLIElement>) => (
-    <li className={cn("mt-2", className)} {...props} />
+    <li className={typefaceListItem(className)} {...props} />
   ),
   blockquote: ({
     className,
     ...props
   }: React.HTMLAttributes<HTMLQuoteElement>) => (
-    <blockquote
-      className={cn(
-        "mt-6 border-l-2 pl-6 italic [&>*]:text-muted-foreground",
-        className,
-      )}
-      {...props}
-    />
+    <blockquote className={typefaceBlockQuote(className)} {...props} />
   ),
   img: ({
     className,
@@ -134,7 +100,7 @@ const components = {
   pre: ({ className, ...props }: React.HTMLAttributes<HTMLPreElement>) => (
     <pre
       className={cn(
-        "mb-4 mt-6 overflow-x-auto rounded-lg border bg-black py-4",
+        "rounded-lg bg-black mb-4 mt-6 overflow-x-auto border py-4",
         className,
       )}
       {...props}
--- src/components/typeface.tsx
+++ src/components/typeface.tsx
@@ -1,105 +1,68 @@
+"use client";
 import { cn } from "@/lib/utils";
-
-// xs: ["0.75rem", "1rem"], // 12px - m2 | m2
-// sm: ["0.875rem", "1.25rem"], // 14px - m1 | b2, m1
-// base: ["1rem", "1.5rem"], // 16px - b2, h6 | h6, b1
-// lg: ["1.125rem", "1.75rem"], // 18px - b1 | h5
-// xl: ["1.25rem", "1.75"], // 20px - h5 | h4
-// "2xl": ["1.5rem", "2rem"], // 24px - h4 | h3
-// "3xl": ["1.75rem", "1"], // 28px - | h2
-// "4xl": ["2rem", "1"], // 32px - h3 | h1
-// "5xl": ["2.5rem", "1"], //40px - h2 |
-// "6xl": ["2.625rem", "1"], // 42px | d2
-// "7xl": ["3rem", "1"], // 48px -  h1 | d1
-// "8xl": ["4rem", "1"], // 64px - d2 |
-// "9xl": ["5rem", "1"], // 80px - d1 |
 
 // Core Typefaces
 
 export const typefaceDisplay1 = (className?: string) =>
-  cn(
-    "font-caecilia text-7xl font-normal leading-normal md:text-9xl",
-    className,
-  );
+  cn("text-9xl font-normal leading-normal", className);
 
 export const typefaceDisplay2 = (className?: string) =>
-  cn(
-    "font-caecilia text-6xl font-normal leading-normal md:text-8xl",
-    className,
-  );
+  cn("text-8xl font-normal leading-normal", className);
 
 export const typefaceHeading1 = (className?: string) =>
-  cn(
-    "font-caecilia text-4xl font-normal leading-normal md:text-7xl",
-    className,
-  );
+  cn("mt-2 scroll-m-20 text-4xl font-bold tracking-tight", className);
 
 export const typefaceHeading2 = (className?: string) =>
   cn(
-    "font-caecilia text-3xl font-normal leading-normal md:text-4xl lg:text-5xl",
+    "mt-10 scroll-m-20 border-b pb-1 text-3xl font-semibold tracking-tight first:mt-0",
     className,
   );
 
 export const typefaceHeading3 = (className?: string) =>
-  cn(
-    "font-caecilia text-2xl font-normal leading-normal md:text-4xl",
-    className,
-  );
+  cn("mt-8 scroll-m-20 text-2xl font-semibold tracking-tight", className);
 
 export const typefaceHeading4 = (className?: string) =>
-  cn("font-caecilia text-xl font-normal leading-normal md:text-2xl", className);
+  cn("mt-8 scroll-m-20 text-xl font-semibold tracking-tight", className);
 
 export const typefaceHeading5 = (className?: string) =>
-  cn("font-caecilia text-lg font-normal leading-normal md:text-xl", className);
+  cn("mt-8 scroll-m-20 text-lg font-semibold tracking-tight", className);
 
 export const typefaceHeading6 = (className?: string) =>
-  cn("font-caecilia text-base font-normal leading-normal", className);
+  cn("mt-8 scroll-m-20 text-base font-semibold tracking-tight", className);
 
 export const typefaceBody1 = (className?: string) =>
-  cn("font-bernini text-base font-normal md:text-lg", className);
+  cn("leading-7 [&:not(:first-child)]:mt-6", className);
 
 export const typefaceBody2 = (className?: string) =>
-  cn("font-bernini text-sm font-normal md:text-base", className);
+  cn("text-sm font-normal", className);
 
 export const typefaceMeta1 = (className?: string) =>
-  cn("font-bernini text-sm font-normal", className);
+  cn("text-sm font-normal", className);
 
 export const typefaceMeta2 = (className?: string) =>
-  cn("font-bernini text-xs font-normal", className);
+  cn("text-xs font-normal", className);
 
 // Component Typefaces
 
-export const typefaceNavigation = (className?: string) =>
-  cn("font-caecilia text-lg font-bold md:text-base", className);
+export const typefaceBlockQuote = (className?: string) =>
+  cn("mt-6 border-l-2 pl-6 italic [&>*]:text-muted-foreground", className);
 
-export const typefaceNavigationSecondary = (className?: string) =>
-  cn("font-bernini text-sm font-normal md:text-xs", className);
+export const typefaceListItem = (className?: string) => cn("mt-2", className);
 
-export const typefaceButton = (className?: string) =>
-  cn("font-caecilia text-base font-normal", className);
+export const typefaceOList = (className?: string) =>
+  cn("my-6 ml-6 list-decimal", className);
 
-export const typefaceBodyCard = (className?: string) =>
-  cn("font-bernini text-base font-normal", className);
+export const typefaceUList = (className?: string) =>
+  cn("my-6 ml-6 list-disc", className);
 
-export const typefaceHeadingSubline = (className?: string) =>
-  cn("font-bernini text-lg font-normal md:text-2xl", className);
+export const typefaceAnchor = (className?: string) =>
+  cn("font-medium underline underline-offset-4", className);
 
 export const typefaceValue1 = (className?: string) =>
-  cn(
-    "font-bernini text-3xl font-normal md:text-4xl lg:text-5xl",
-    "leading-none",
-    className,
-  );
+  cn("text-3xl font-normal", "leading-none", className);
 
 export const typefaceValue2 = (className?: string) =>
-  cn(
-    "font-bernini text-2xl font-normal md:text-3xl lg:text-4xl",
-    "leading-none",
-    className,
-  );
+  cn("text-2xl font-normal", "leading-none", className);
 
 export const typefaceValue3 = (className?: string) =>
-  cn("font-bernini text-lg font-normal md:text-xl", "leading-none", className);
-
-export const typefaceChapterNav = (className?: string) =>
-  cn("font-caecilia text-base font-normal md:text-4xl", className);
+  cn("text-lg font-normal", "leading-none", className);

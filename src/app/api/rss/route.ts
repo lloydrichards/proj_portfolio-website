d7f329f7660a5e5e4f7f02acc76e5c644925e009
lloydrichards <lloyd.d.richards@gmail.com>
2024-05-20 09:18:23 +0200

    fix(routing): :bug: rebuild rss api

--- src/app/api/rss/route.ts
+++ src/app/api/rss/route.ts
@@ -1,40 +1,34 @@
+import { metadata } from "@/app/layout";
 import { getBaseUrl } from "@/lib/utils";
-import { getAllLabs } from "@/service/get-all-lab";
+import { getAllPosts } from "@/service/get-all-posts";
 
 export async function GET() {
-  const allLabs = await getAllLabs();
+  const blogPosts = await getAllPosts();
 
-  const labItemsXml = allLabs
-    .sort((a, b) => {
-      if (new Date(a.date) > new Date(b.date)) {
-        return -1;
-      }
-      return 1;
-    })
-    .map(
-      (lab) =>
-        `<item>
-          <title>${lab.title}</title>
-          <link>${getBaseUrl()}${lab.slug}</link>
-          <description>${lab.description || ""}</description>
-          <pubDate>${new Date(lab.date).toUTCString()}</pubDate>
-        </item>`,
-    )
-    .join("\n");
+  const feed = `<?xml version="1.0" encoding="UTF-8" ?>
+    <rss version="2.0">
+        <channel>
+            <title>${metadata.title}</title>
+            <link>${getBaseUrl()}</link>
+            <description>${metadata.description}</description>
+            <language>en-us</language>
+            ${blogPosts
+              .map(
+                (post) => `
+            <item>
+                <title>${post.title}</title>
+                <link>${getBaseUrl()}${post.slug}</link>
+                <description>${post.description}</description>
+                <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+            </item>
+            `,
+              )
+              .join("")}
+        </channel>
+    </rss>`;
 
-  const rssFeed = `<?xml version="1.0" encoding="UTF-8" ?>
-  <rss version="2.0">
-    <channel>
-        <title>My Portfolio</title>
-        <link>${getBaseUrl()}</link>
-        <description>This is my portfolio RSS feed</description>
-        ${labItemsXml}
-    </channel>
-  </rss>`;
-
-  return new Response(rssFeed, {
-    headers: {
-      "Content-Type": "text/xml",
-    },
+  return new Response(feed, {
+    status: 200,
+    headers: { "Content-Type": "application/rss+xml" },
   });
 }

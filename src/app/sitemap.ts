0f131c5fa33b5fae37083be42352578f11d47e5f
lloydrichards <lloyd.d.richards@gmail.com>
2025-01-05 20:29:54 +0100

    perf: :zap: better sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,19 +1,23 @@
+import { getAllLabs } from "@/services/get-all-labs";
 import { getAllProjects } from "@/services/get-all-projects";
 
 export const BASE_URL = "https://lloydrichards.dev";
 
 export default async function sitemap() {
   const projects = await getAllProjects();
+  const labs = await getAllLabs();
 
-  const notes = projects.map(({ frontmatter }) => ({
+  const notes = [...projects, ...labs].map(({ frontmatter }) => ({
     url: BASE_URL + frontmatter.pathname,
     lastModified: frontmatter.lastModified,
   }));
 
-  const routes = ["", "/project"].map((route) => ({
-    url: BASE_URL + route,
-    lastModified: new Date().toISOString(),
-  }));
+  const routes = ["", "/about", "/project", "/lab", "/timeline"].map(
+    (route) => ({
+      url: BASE_URL + route,
+      lastModified: new Date().toISOString(),
+    }),
+  );
 
   return [...routes, ...notes];
 }

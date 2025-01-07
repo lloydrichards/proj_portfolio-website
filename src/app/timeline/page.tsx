5d49a670c188860b648409fbc8e54a69b7fc1763
lloydrichards <lloyd.d.richards@gmail.com>
2025-01-07 22:13:47 +0100

    style: :recycle: tidy up timeline page

--- src/app/timeline/page.tsx
+++ src/app/timeline/page.tsx
@@ -1,6 +1,5 @@
 import { Timeline } from "@/components/chart/timeline/Timeline";
 import { ResponsiveWrapper } from "@/components/template/responsive_wrapper";
-import { typefaceHeading1 } from "@/components/tokens/typeface";
 import { getAllOccupations } from "@/services/get-all-occupations";
 import { createPageMetadata } from "@/lib/seo";
 
@@ -13,8 +12,7 @@ const TimelinePage: React.FC = async () => {
   const allOccupations = await getAllOccupations();
   const data = allOccupations.map((o) => o.frontmatter);
   return (
-    <main className="col-span-full">
-      <h1 className={typefaceHeading1()}>Welcome to the timeline page</h1>
+    <main className="col-span-full py-6">
       <ResponsiveWrapper>
         <Timeline data={data} />
       </ResponsiveWrapper>

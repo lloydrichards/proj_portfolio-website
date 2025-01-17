6caddd16bf3dcc81243b9db4c8db6730b55bf15a
lloydrichards <lloyd.d.richards@gmail.com>
2025-01-17 10:03:19 +0100

    fix: :card_file_box: change path on server

--- src/services/db/index.ts
+++ src/services/db/index.ts
@@ -3,7 +3,7 @@ import { drizzle } from "drizzle-orm/libsql";
 
 import { env } from "@/lib/env";
 import * as schema from "./schema";
-
+import path from "path";
 /**
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
@@ -12,8 +12,10 @@ const globalForDb = globalThis as unknown as {
   client: Client | undefined;
 };
 
+const dbPath = path.join(process.cwd(), env.DB_FILE_NAME);
+
 export const client =
-  globalForDb.client ?? createClient({ url: `file:${env.DB_FILE_NAME}` });
+  globalForDb.client ?? createClient({ url: `file:${dbPath}` });
 if (env.NODE_ENV !== "production") globalForDb.client = client;
 
 export const db = drizzle(client, { schema });

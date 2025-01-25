82a28af4069a15079b0c27f27fabacb83ac0b0d3
lloydrichards <lloyd.d.richards@gmail.com>
2025-01-25 23:25:03 +0100

    fix: :bug: skip client initialization on edge

--- src/services/db/index.ts
+++ src/services/db/index.ts
@@ -9,10 +9,17 @@ const globalForDb = globalThis as unknown as {
   client: Client | undefined;
 };
 
-const dbPath = path.join(process.cwd(), env.DB_FILE_NAME);
+const initClient = () => {
+  if (env.NEXT_RUNTIME === "edge") {
+    // Skip database initialization in edge runtime
+    return null as unknown as Client;
+  }
+  const dbPath = path.join(process.cwd(), env.DB_FILE_NAME);
+  return globalForDb.client ?? createClient({ url: `file:${dbPath}` });
+};
 
-export const client =
-  globalForDb.client ?? createClient({ url: `file:${dbPath}` });
+export const client = initClient();
+
 if (env.NODE_ENV !== "production") globalForDb.client = client;
 
 export const db = drizzle(client, { schema });

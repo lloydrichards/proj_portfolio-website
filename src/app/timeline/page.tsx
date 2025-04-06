954cebdd77b45d029b3a25071a73416d17603ca2
lloydrichards <lloyd.d.richards@gmail.com>
2025-04-06 14:33:23 +0200

    fix: :wheelchair: add header for accessibility

--- src/app/timeline/page.tsx
+++ src/app/timeline/page.tsx
@@ -2,6 +2,7 @@ import { Tile } from "@/components/atom/tile";
 import { Timeline } from "@/components/chart/timeline/timeline";
 import { OccupationCard } from "@/components/molecule/occupation_card";
 import { ResponsiveWrapper } from "@/components/template/responsive_wrapper";
+import { typefaceHeading1 } from "@/components/tokens/typeface";
 import { createPageMetadata } from "@/lib/seo";
 import { DataApi } from "@/services/DataApi";
 import { RuntimeServer } from "@/services/RuntimeServer";
@@ -20,7 +21,10 @@ const TimelinePage: React.FC = async () => {
     ),
   );
   return (
-    <>
+    <main className="contents">
+      <Tile size="unset" className="col-span-full">
+        <h1 className={typefaceHeading1("p-2")}>Timeline</h1>
+      </Tile>
       <aside className="col-span-1 grid grid-cols-subgrid md:col-span-2 lg:col-span-4">
         <Tile className="col-span-full h-full" size="unset">
           <ResponsiveWrapper>
@@ -28,14 +32,18 @@ const TimelinePage: React.FC = async () => {
           </ResponsiveWrapper>
         </Tile>
       </aside>
-      <main className="mosaic-rows col-[2/-1] grid min-h-dvh grid-cols-subgrid md:col-[3/-1] lg:col-[5/-1]">
+      <section className="mosaic-rows col-[2/-1] grid min-h-dvh grid-cols-subgrid md:col-[3/-1] lg:col-[5/-1]">
         {allOccupations.map((o) => (
-          <Tile className="col-span-full row-span-3 md:row-span-5 lg:row-span-6 xl:row-span-5" size="unset" key={o.id}>
+          <Tile
+            className="col-span-full row-span-3 md:row-span-5 lg:row-span-6 xl:row-span-5"
+            size="unset"
+            key={o.id}
+          >
             <OccupationCard data={o} />
           </Tile>
         ))}
-      </main>
-    </>
+      </section>
+    </main>
   );
 };
 

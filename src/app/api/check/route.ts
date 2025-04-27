5d6b5c18f3f6baca30d217bb1b8546ed3e89410e
lloydrichards <lloyd.d.richards@gmail.com>
2025-04-27 21:33:44 +0200

    .

--- src/app/api/check/route.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "@/services/db";
-import { occupation } from "@/services/db/schema";
-
-export const revalidate = 0;
-
-export async function GET() {
-  const data = await db.select().from(occupation);
-  return Response.json({ data });
-}

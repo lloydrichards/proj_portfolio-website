483828ac2552d6c1936b97414c94d1bb26642049
Lloyd Richards <lloyd.d.richards@gmail.com>
2025-11-30 15:40:44 +0100

    refactor: select for navigation on mobile

--- src/app/labs/lab_navigation.tsx
+++ src/app/labs/lab_navigation.tsx
@@ -1,10 +1,18 @@
 "use client";
+import { usePathname, useRouter } from "next/navigation";
 import type { FC } from "react";
 import { Badge } from "@/components/atom/badge";
 import {
   NavigationMenu,
   NavigationMenuList,
 } from "@/components/atom/navigation-menu";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/atom/select";
 import { NavListItem } from "@/components/molecule/nav_list_item";
 import { cn } from "@/lib/utils";
 import type { Lab } from "@/types/Lab";
@@ -14,34 +22,65 @@ interface LabNavigationProps {
   className?: string;
 }
 export const LabNavigation: FC<LabNavigationProps> = ({ labs, className }) => {
-  return (
-    <NavigationMenu
-      aria-label="Lab Navigation"
-      orientation="vertical"
-      className="contents"
-    >
-      <NavigationMenuList className="contents">
-        <NavListItem
-          href="/labs"
-          exact
-          className={cn("col-span-full row-span-2 border", className)}
-        >
-          <span className="w-full text-center">All Labs</span>
-        </NavListItem>
+  const pathname = usePathname();
+  const router = useRouter();
 
-        {labs
-          .filter((d) => d.isPublished)
-          .map((p) => (
-            <NavListItem
-              key={p.slug}
-              href={p.pathname}
-              className={cn("col-span-full overflow-visible", className)}
-            >
-              <Badge variant="outline">{p.id}</Badge>
-              <span className="hidden lg:line-clamp-1">{p.title}</span>
-            </NavListItem>
-          ))}
-      </NavigationMenuList>
-    </NavigationMenu>
+  return (
+    <>
+      <NavigationMenu
+        aria-label="Lab Navigation"
+        orientation="vertical"
+        className="contents"
+      >
+        <NavigationMenuList className="contents">
+          <NavListItem
+            href="/labs"
+            exact
+            className={cn(
+              "col-span-1 md:col-span-full md:row-span-2 border",
+              className,
+            )}
+          >
+            <span className="w-full text-center">All Labs</span>
+          </NavListItem>
+
+          {labs
+            .filter((d) => d.isPublished)
+            .map((p) => (
+              <NavListItem
+                key={p.slug}
+                href={p.pathname}
+                className={cn(
+                  "col-span-full overflow-visible hidden md:block",
+                  className,
+                )}
+              >
+                <Badge variant="outline">{p.id}</Badge>
+                <span className="hidden lg:line-clamp-1">{p.title}</span>
+              </NavListItem>
+            ))}
+        </NavigationMenuList>
+      </NavigationMenu>
+      <Select
+        defaultValue={pathname === "/labs" ? "" : pathname.split("/labs/")[1]}
+        onValueChange={(value) => router.push(`/labs/${value}`)}
+      >
+        <SelectTrigger className="h-full border-border col-span-7 md:hidden">
+          <SelectValue placeholder="Select a lab" />
+        </SelectTrigger>
+        <SelectContent>
+          {labs
+            .filter((d) => d.isPublished)
+            .map((p) => (
+              <SelectItem key={p.slug} value={p.slug}>
+                <Badge variant="outline" className="mr-2">
+                  {p.id}
+                </Badge>
+                {p.title}
+              </SelectItem>
+            ))}
+        </SelectContent>
+      </Select>
+    </>
   );
 };
--- src/app/projects/project_navigation.tsx
+++ src/app/projects/project_navigation.tsx
@@ -1,10 +1,18 @@
 "use client";
+import { usePathname, useRouter } from "next/navigation";
 import type { FC } from "react";
 import { Badge } from "@/components/atom/badge";
 import {
   NavigationMenu,
   NavigationMenuList,
 } from "@/components/atom/navigation-menu";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/atom/select";
 import { NavListItem } from "@/components/molecule/nav_list_item";
 import { cn } from "@/lib/utils";
 import type { Project } from "@/types/Project";
@@ -17,33 +25,66 @@ export const ProjectNavigation: FC<ProjectNavigationProps> = ({
   projects,
   className,
 }) => {
+  const pathname = usePathname();
+  const router = useRouter();
+
   return (
-    <NavigationMenu
-      aria-label="Project Navigation"
-      orientation="vertical"
-      className="contents"
-    >
-      <NavigationMenuList className="contents">
-        <NavListItem
-          href="/projects"
-          exact
-          className={cn("col-span-full row-span-2 border", className)}
-        >
-          <span className="w-full text-center">All Projects</span>
-        </NavListItem>
-        {projects
-          .filter((d) => d.isPublished)
-          .map((p) => (
-            <NavListItem
-              key={p.slug}
-              href={p.pathname}
-              className={cn("col-span-full", className)}
-            >
-              <Badge variant="outline">{p.id}</Badge>
-              <span className="hidden lg:line-clamp-1">{p.title}</span>
-            </NavListItem>
-          ))}
-      </NavigationMenuList>
-    </NavigationMenu>
+    <>
+      <NavigationMenu
+        aria-label="Project Navigation"
+        orientation="vertical"
+        className="contents"
+      >
+        <NavigationMenuList className="contents">
+          <NavListItem
+            href="/projects"
+            exact
+            className={cn(
+              "col-span-1 md:col-span-full md:row-span-2 border",
+              className,
+            )}
+          >
+            <span className="w-full text-center">All Projects</span>
+          </NavListItem>
+          {projects
+            .filter((d) => d.isPublished)
+            .map((p) => (
+              <NavListItem
+                key={p.slug}
+                href={p.pathname}
+                className={cn(
+                  "col-span-full overflow-visible hidden md:block",
+                  className,
+                )}
+              >
+                <Badge variant="outline">{p.id}</Badge>
+                <span className="hidden lg:line-clamp-1">{p.title}</span>
+              </NavListItem>
+            ))}
+        </NavigationMenuList>
+      </NavigationMenu>
+      <Select
+        defaultValue={
+          pathname === "/projects" ? "" : pathname.split("/projects/")[1]
+        }
+        onValueChange={(value) => router.push(`/projects/${value}`)}
+      >
+        <SelectTrigger className="h-full border-border col-span-7 md:hidden">
+          <SelectValue placeholder="Select a project" />
+        </SelectTrigger>
+        <SelectContent>
+          {projects
+            .filter((d) => d.isPublished)
+            .map((p) => (
+              <SelectItem key={p.slug} value={p.slug}>
+                <Badge variant="outline" className="mr-2">
+                  {p.id}
+                </Badge>
+                {p.title}
+              </SelectItem>
+            ))}
+        </SelectContent>
+      </Select>
+    </>
   );
 };

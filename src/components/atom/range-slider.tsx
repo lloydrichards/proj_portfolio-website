9ca0a4ad3599e19cf78ef50430698bce95a11c9b
Lloyd Richards <lloyd.d.richards@gmail.com>
2026-02-20 21:44:21 +0100

    refactor: base-ui slider and range-slider

--- src/components/atom/range-slider.tsx
+++ src/components/atom/range-slider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import * as SliderPrimitive from "@radix-ui/react-slider";
+import { Slider as SliderPrimitive } from "@base-ui/react/slider";
 import { format } from "date-fns";
 import * as React from "react";
 import { cn } from "@/lib/utils";
@@ -21,10 +21,7 @@ interface RangeSliderProps {
   onValueChange?: (value: [Date, Date]) => void;
 }
 
-const DateRangeSlider = React.forwardRef<
-  React.ElementRef<typeof SliderPrimitive.Root>,
-  RangeSliderProps
->(
+const DateRangeSlider = React.forwardRef<HTMLDivElement, RangeSliderProps>(
   (
     {
       className,
@@ -37,6 +34,9 @@ const DateRangeSlider = React.forwardRef<
       defaultValue,
       step = 86400000,
       onValueChange,
+      disabled,
+      orientation,
+      dir,
       ...props
     },
     ref,
@@ -81,28 +81,35 @@ const DateRangeSlider = React.forwardRef<
         value={convertedValue}
         defaultValue={convertedDefaultValue}
         onValueChange={handleValueChange}
+        disabled={disabled}
+        orientation={orientation}
+        dir={dir}
         {...props}
       >
-        <SliderPrimitive.Track className="bg-input relative h-2 w-full grow overflow-hidden rounded-full">
-          <SliderPrimitive.Range className="bg-primary absolute h-full" />
-        </SliderPrimitive.Track>
-        {initialValue.map((timestamp, index) => (
-          <React.Fragment key={index}>
-            <SliderPrimitive.Thumb className="border-primary bg-background ring-offset-background focus-visible:ring-ring relative block h-4 w-4 rounded-full border-2 transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50">
-              {label && (
-                <span
-                  className={cn(
-                    "absolute flex w-full justify-center whitespace-nowrap",
-                    labelPosition === "top" && "-top-7",
-                    labelPosition === "bottom" && "top-4",
-                  )}
-                >
-                  {label ? label(new Date(timestamp)) : defaultLabel(timestamp)}
-                </span>
-              )}
-            </SliderPrimitive.Thumb>
-          </React.Fragment>
-        ))}
+        <SliderPrimitive.Control className="grid grow data-[orientation=horizontal]:h-2 data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-2">
+          <SliderPrimitive.Track className="bg-input relative overflow-hidden rounded-full data-[orientation=horizontal]:h-full data-[orientation=vertical]:w-full">
+            <SliderPrimitive.Indicator className="bg-primary absolute data-[orientation=horizontal]:h-full data-[orientation=vertical]:w-full" />
+          </SliderPrimitive.Track>
+          {initialValue.map((timestamp) => (
+            <React.Fragment key={`range-slider-thumb-${timestamp}`}>
+              <SliderPrimitive.Thumb className="border-primary bg-background ring-offset-background focus-visible:ring-ring relative block h-4 w-4 rounded-full border-2 transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50">
+                {label && (
+                  <span
+                    className={cn(
+                      "absolute flex w-full justify-center whitespace-nowrap",
+                      labelPosition === "top" && "-top-7",
+                      labelPosition === "bottom" && "top-4",
+                    )}
+                  >
+                    {label
+                      ? label(new Date(timestamp))
+                      : defaultLabel(timestamp)}
+                  </span>
+                )}
+              </SliderPrimitive.Thumb>
+            </React.Fragment>
+          ))}
+        </SliderPrimitive.Control>
       </SliderPrimitive.Root>
     );
   },

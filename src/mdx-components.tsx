ea1f15e70e66bb80a5e23e5076737130007c65e9
Lloyd Richards <lloyd.d.richards@gmail.com>
2026-06-10 09:48:59 +0200

    docs: add 2600 project

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,3 +1,4 @@
+import { AnsiTerminal } from "@/components/atom/ansi-terminal";
 import { GitHubCommitCardServer } from "./components/molecule/github_commit_card.server";
 import { Mermaid } from "./components/molecule/mermaid";
 import { proseComponents } from "./components/tokens/prose_components";
@@ -6,6 +7,7 @@ export const components = {
   ...proseComponents,
   Mermaid,
   mermaid: Mermaid,
+  AnsiTerminal,
   GitHubCommitCard: GitHubCommitCardServer,
 };
 

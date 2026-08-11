e8a5892d135656390f9d422778ee6c50da111bf9
Lloyd Richards <lloyd.d.richards@gmail.com>
2026-08-11 12:57:21 +0200

    feat: add expandable image and mermaid components

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,10 +1,12 @@
 import { AnsiTerminal } from "@/components/atom/ansi-terminal";
+import { ExpandableImage } from "./components/molecule/expandable-image";
 import { GitHubCommitCardServer } from "./components/molecule/github_commit_card.server";
 import { Mermaid } from "./components/molecule/mermaid";
 import { proseComponents } from "./components/tokens/prose_components";
 
 export const components = {
   ...proseComponents,
+  img: ExpandableImage,
   Mermaid,
   mermaid: Mermaid,
   AnsiTerminal,
